6d8eef5f697a2f2eb9978ac0ae81618bf159868b
wlsh44 <k87913j@gmail.com>
2022-04-06 01:00:12 +0900

    feat: getCareer 메서드 추가 #2

--- src/main/java/flab/project/jobfinder/service/crawler/JobKoreaCrawlerService.java
+++ src/main/java/flab/project/jobfinder/service/crawler/JobKoreaCrawlerService.java
@@ -12,22 +12,41 @@ import static flab.project.jobfinder.util.JobKoreaConst.JOBKOREA_URL;
 public class JobKoreaCrawlerService implements CrawlerService {
 
     private String detailedSearchQueryParams(DetailedSearchDto dto) {
-        StringBuilder queryParams = new StringBuilder("?stext=");
+        StringBuilder queryParams = new StringBuilder("stext=");
         String location = getLocation(dto);
-
+        String career = getCareer(dto);
         queryParams.append(dto.getSearchText()).append(location);
         return queryParams.toString();
     }
 
     private String getLocation(DetailedSearchDto dto) {
-        if (dto.getLocation() != null) {
-            String location = dto.getLocation().stream()
-                    .map(Location::jobkoreaCode)
-                    .collect(Collectors.joining(JOBKOREA_DELIMITER));
-            return "&local=" + location;
-        } else {
+        if (dto.getLocation() == null) {
             return "";
         }
+        String location = dto.getLocation().stream()
+                .map(Location::jobkoreaCode)
+                .collect(Collectors.joining(JOBKOREA_DELIMITER));
+        return "&local=" + location;
+    }
+
+    private String getCareer(DetailedSearchDto dto) {
+        if (dto.getCareer() == null) {
+            return "";
+        }
+        StringBuilder params = new StringBuilder();
+        DetailedSearchDto.Career career = dto.getCareer();
+
+        if (career.getCareerType() != null) {
+            params.append("&careerType=").append(career.getCareerType().jobkoreaCode());
+        }
+        if (career.getCareerMin() != null) {
+            params.append("&careerMin=").append(career.getCareerMin());
+        }
+        if (career.getCareerMax() != null) {
+            params.append("&careerMax=").append(career.getCareerMax());
+        }
+        return params.toString();
+
     }
 
     @Override
