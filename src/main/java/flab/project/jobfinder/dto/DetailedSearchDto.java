be2cdc25ed79293dabda03f42947ad331a923194
wlsh44 <k87913j@gmail.com>
2022-04-05 17:09:19 +0900

    feat: DetailedSearchDto 추가 #2

---
+++ src/main/java/flab/project/jobfinder/dto/DetailedSearchDto.java
@@ -0,0 +1,50 @@
+package flab.project.jobfinder.dto;
+
+import flab.project.jobfinder.util.CarrerType;
+import flab.project.jobfinder.util.JobType;
+import flab.project.jobfinder.util.Location;
+import flab.project.jobfinder.util.PayType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Optional;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class DetailedSearchDto {
+
+    private final String searchText;
+    private final List<Location> location;
+    private final List<JobType> jobType;
+    private final Pay pay;
+    private final Career career;
+
+    @Getter
+    public static class Pay {
+        private final PayType payType;
+        private final String minPay;
+        private final String maxPay;
+
+        public Pay(PayType payType, String minPay, String maxPay) {
+            this.payType = payType;
+            this.minPay = minPay;
+            this.maxPay = maxPay;
+        }
+    }
+
+    @Getter
+    public static class Career {
+        private final CarrerType careerType;
+        private final String minCareer;
+        private final String maxCareer;
+
+        public Career(CarrerType careerType, String minCareer, String maxCareer) {
+            this.careerType = careerType;
+            this.minCareer = minCareer;
+            this.maxCareer = maxCareer;
+        }
+    }
+}
