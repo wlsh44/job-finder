fce7e14efb94bc4154fcecea6d5c3e21262971d0
wlsh44 <k87913j@gmail.com>
2022-04-20 17:45:36 +0900

    test: 필요 없는 코드 삭제 #2

--- src/test/java/flab/project/jobfinder/service/crawler/JobKoreaCrawlerServiceTest.java
+++ src/test/java/flab/project/jobfinder/service/crawler/JobKoreaCrawlerServiceTest.java
@@ -9,17 +9,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@EnableConfigurationProperties(value = JobKoreaPropertiesConfig.class)
-@TestPropertySource("classpath:application-test.properties")
 class JobKoreaCrawlerServiceTest {
 
     @InjectMocks
