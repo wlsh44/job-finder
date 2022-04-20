4913a94709d8f52cda0cf8f7ba79227733e7aff7
wlsh44 <k87913j@gmail.com>
2022-04-20 18:01:08 +0900

    chore: 필요없는 코드 삭제 #2

--- src/main/java/flab/project/jobfinder/JobFinderApplication.java
+++ src/main/java/flab/project/jobfinder/JobFinderApplication.java
@@ -1,12 +1,9 @@
 package flab.project.jobfinder;
 
-import flab.project.jobfinder.config.JobKoreaPropertiesConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JobKoreaPropertiesConfig.class)
 public class JobFinderApplication {
 
     public static void main(String[] args) {
