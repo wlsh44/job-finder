2bef4e82205cd313ead655ed8eda9961613b5d9c
wlsh44 <k87913j@gmail.com>
2022-04-20 17:53:27 +0900

    refactor: 추상화된 객체 의존성 주입 #2

--- src/main/java/flab/project/jobfinder/service/crawler/JobKoreaCrawlerService.java
+++ src/main/java/flab/project/jobfinder/service/crawler/JobKoreaCrawlerService.java
@@ -2,22 +2,28 @@ package flab.project.jobfinder.service.crawler;
 
 import flab.project.jobfinder.config.JobKoreaPropertiesConfig;
 import flab.project.jobfinder.dto.DetailedSearchDto;
-import flab.project.jobfinder.service.crawler.generator.JobKoreaQueryParamGenerator;
-import lombok.RequiredArgsConstructor;
+import flab.project.jobfinder.service.crawler.generator.QueryParamGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class JobKoreaCrawlerService implements CrawlerService {
 
-    private final JobKoreaQueryParamGenerator paramGenerator;
+    private final QueryParamGenerator paramGenerator;
     private final JobKoreaPropertiesConfig config;
+
+    @Autowired
+    public JobKoreaCrawlerService(@Qualifier("jobKoreaQueryParamGenerator") QueryParamGenerator paramGenerator, JobKoreaPropertiesConfig config) {
+        this.paramGenerator = paramGenerator;
+        this.config = config;
+    }
 
     @Override
     public Document crawl(DetailedSearchDto dto) {
