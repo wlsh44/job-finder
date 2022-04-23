f5f3804463e22f8228b8c95935b0c568921c32ec
wlsh44 <k87913j@gmail.com>
2022-04-23 16:08:05 +0900

    fix: parse 파라미터 변경 #2

--- src/main/java/flab/project/jobfinder/service/perser/ParserService.java
+++ src/main/java/flab/project/jobfinder/service/perser/ParserService.java
@@ -1,8 +1,8 @@
 package flab.project.jobfinder.service.perser;
 
+import flab.project.jobfinder.dto.DetailedSearchDto;
 import flab.project.jobfinder.dto.ParseDto;
-import org.jsoup.nodes.Document;
 
 public interface ParserService {
-    ParseDto parse(Document doc);
+    ParseDto parse(DetailedSearchDto dto);
 }
