6f625a984a9c313477b0e8e61342bda8eda0c605
wlsh44 <k87913j@gmail.com>
2022-04-23 15:45:33 +0900

    feat: parseDto 추가 #2

---
+++ src/main/java/flab/project/jobfinder/dto/ParseDto.java
@@ -0,0 +1,14 @@
+package flab.project.jobfinder.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ParseDto {
+    private String title;
+    private String url;
+    private String location;
+    private String pay;
+    private String techStack;
+    private String jobType;
+    private String Career;
+}
