a76ffa56b51484553d526d89b994c021acbe99ac
wlsh44 <k87913j@gmail.com>
2022-05-09 02:33:20 +0900

    refactor: searchFormDto를 view에 전달하도록 변경 #5

--- src/main/java/flab/project/jobfinder/controller/JobFinderController.java
+++ src/main/java/flab/project/jobfinder/controller/JobFinderController.java
@@ -3,6 +3,7 @@ package flab.project.jobfinder.controller;
 import flab.project.jobfinder.dto.DetailedSearchDto;
 import flab.project.jobfinder.dto.RecruitDto;
 import flab.project.jobfinder.dto.RecruitPageDto;
+import flab.project.jobfinder.dto.SearchFormDto;
 import flab.project.jobfinder.enums.Location;
 import flab.project.jobfinder.enums.Platform;
 import flab.project.jobfinder.service.JobFindFactory;
@@ -12,8 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
@@ -25,30 +26,32 @@ public class JobFinderController {
 
     @GetMapping("/job-find")
     public String getJobFindForm(Model model) {
-        DetailedSearchDto searchFormDto = new DetailedSearchDto();
-        Integer currentPage = 1;
+        SearchFormDto searchFormDto = new SearchFormDto();
 
         model.addAttribute("searchFormDto", searchFormDto);
         model.addAttribute("locationMap", Location.getDistrictMap());
         model.addAttribute("platformMap", Platform.getKoreaNameMap());
-        model.addAttribute("currentPage", currentPage);
         return "form";
     }
 
     @PostMapping("/job-find")
-    public String jobFind(Model model, DetailedSearchDto searchFormDto, @RequestParam Integer currentPage) {
+    public String jobFind(@Valid SearchFormDto searchFormDto, Model model) {
+        DetailedSearchDto detailedSearchDto = searchFormDto.getDetailedSearchDto();
+        Integer currentPage = searchFormDto.getCurrentPage();
+
         log.info(searchFormDto.toString());
-        RecruitPageDto recruitPageDto = jobFindFactory.getRecruitPageDto(searchFormDto, currentPage);
+        RecruitPageDto recruitPageDto = jobFindFactory.getRecruitPageDto(detailedSearchDto, currentPage);
 
         List<RecruitDto> recruitDtoList = recruitPageDto.getRecruitDtoList();
         int totalPage = recruitPageDto.getTotalPage();
         int startPage = recruitPageDto.getStartPage();
 
+        SearchFormDto newSearchFormDto = new SearchFormDto(detailedSearchDto, currentPage);
+
         model.addAttribute("recruitDtoList", recruitDtoList);
         model.addAttribute("startPage", startPage);
         model.addAttribute("totalPage", totalPage);
-        model.addAttribute("currentPage", currentPage);
-        model.addAttribute("searchFormDto", searchFormDto);
+        model.addAttribute("searchFormDto", newSearchFormDto);
         return "recruits";
     }
 }
