0b502f8107185a32f20763806c82a74f889d17c5
wlsh44 <k87913j@gmail.com>
2022-05-17 15:16:44 +0900

    test: 로켓펀치 paramGenerator 테스트 코드 수정 #3

--- src/test/java/flab/project/jobfinder/service/crawler/generator/RocketPunchQueryParamGeneratorTest.java
+++ src/test/java/flab/project/jobfinder/service/crawler/generator/RocketPunchQueryParamGeneratorTest.java
@@ -1,23 +1,19 @@
 package flab.project.jobfinder.service.crawler.generator;
 
-import flab.project.jobfinder.config.JobKoreaPropertiesConfig;
 import flab.project.jobfinder.config.RocketPunchPropertiesConfig;
 import flab.project.jobfinder.dto.DetailedSearchDto;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
+import static flab.project.jobfinder.enums.CareerType.ANY;
 import static flab.project.jobfinder.enums.CareerType.JUNIOR;
 import static flab.project.jobfinder.enums.JobType.MILITARY;
 import static flab.project.jobfinder.enums.Location.*;
@@ -27,7 +23,6 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ExtendWith(MockitoExtension.class)
 class RocketPunchQueryParamGeneratorTest {
 
     RocketPunchQueryParamGenerator rocketPunchQueryParamGenerator;
@@ -45,26 +40,53 @@ class RocketPunchQueryParamGeneratorTest {
     @ParameterizedTest(name = "{index} => {1}")
     @MethodSource("provideDto")
     @DisplayName("정상 url 리턴")
-    void 정상_url_리턴(DetailedSearchDto dto, String expected) {
-        String result =  rocketPunchQueryParamGenerator.toQueryParams(dto, 1);
+    void 정상_url_리턴(DetailedSearchDto dto, MultiValueMap<String, String> expected) {
+        MultiValueMap<String, String> result =  rocketPunchQueryParamGenerator.toQueryParams(dto, 1);
 
         assertThat(result).isEqualTo(expected);
     }
 
     private Stream<Arguments> provideDto() {
+        MultiValueMap<String, String> map1 = new LinkedMultiValueMap<>(Map.of(
+                "page", List.of("1")));
+        MultiValueMap<String, String> map2 = new LinkedMultiValueMap<>(Map.of(
+                "keywords", List.of("spring"),
+                "page", List.of("1")));
+        MultiValueMap<String, String> map3 = new LinkedMultiValueMap<>(Map.of(
+                "keywords", List.of("spring boot"),
+                "career_type", List.of("1"),
+                "page", List.of("1")));
+        MultiValueMap<String, String> map4 = new LinkedMultiValueMap<>(Map.of(
+                "keywords", List.of("react 웹 프런트"),
+                "location", List.of("동대문구&location=은평구&location=용산구"),
+                "hiring_types", List.of("3"),
+                "page", List.of("1")));
+        MultiValueMap<String, String> map5 = new LinkedMultiValueMap<>(Map.of(
+                "keywords", List.of("웹 서비스"),
+                "location", List.of("강남구&location=분당구"),
+                "salary", List.of("40000000-"),
+                "page", List.of("1")));
+        MultiValueMap<String, String> map6 = new LinkedMultiValueMap<>(Map.of(
+                "keywords", List.of("테스트"),
+                "page", List.of("1")));
+
         return Stream.of(
-                Arguments.of(DetailedSearchDto.builder().searchText("spring").build(), "keywords=spring&page=1"),
+                Arguments.of(DetailedSearchDto.builder().build(), map1),
+                Arguments.of(DetailedSearchDto.builder().searchText("spring").build(), map2),
                 Arguments.of(DetailedSearchDto.builder().searchText("spring boot")
                                 .career(new DetailedSearchDto.Career(JUNIOR, null, 2)).build()
-                        , "keywords=spring+boot&career_type=1&page=1"),
-                Arguments.of(DetailedSearchDto.builder().searchText("웹 서비스")
-                                .location(List.of(GANGNAM, BUNDANG))
-                                .pay(new DetailedSearchDto.Pay(ANNUAL, 4000, null)).build()
-                        , "keywords=%EC%9B%B9+%EC%84%9C%EB%B9%84%EC%8A%A4&location=%EA%B0%95%EB%82%A8%EA%B5%AC&location=%EB%B6%84%EB%8B%B9%EA%B5%AC&salary=40000000-200000000&page=1"),
+                        , map3),
                 Arguments.of(DetailedSearchDto.builder().searchText("react 웹 프런트")
                                 .location(List.of(DONGDAEMUN, EUNPYEONG, YONGSAN))
                                 .jobType(List.of(MILITARY)).build()
-                        , "keywords=react+%EC%9B%B9+%ED%94%84%EB%9F%B0%ED%8A%B8&location=%EB%8F%99%EB%8C%80%EB%AC%B8%EA%B5%AC&location=%EC%9D%80%ED%8F%89%EA%B5%AC&location=%EC%9A%A9%EC%82%B0%EA%B5%AC&hiring_types=3&page=1")
+                        , map4),
+                Arguments.of(DetailedSearchDto.builder().searchText("웹 서비스")
+                                .location(List.of(GANGNAM, BUNDANG))
+                                .pay(new DetailedSearchDto.Pay(ANNUAL, 4000, null)).build()
+                        , map5),
+                Arguments.of(DetailedSearchDto.builder().searchText("테스트")
+                                .career(new DetailedSearchDto.Career(ANY, 1, null)).build()
+                        , map6)
         );
     }
 }
