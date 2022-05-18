5fb239fdc975a2d073a1afa3c852d0e39bba7872
wlsh44 <k87913j@gmail.com>
2022-05-18 18:28:31 +0900

    test: 테스트 코드 이름 변경 #3

--- src/test/java/flab/project/jobfinder/controller/JobKoreaJobFinderControllerTest.java
+++ src/test/java/flab/project/jobfinder/controller/JobKoreaJobFinderControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -26,8 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(JobFinderController.class)
-@ActiveProfiles("dev")
-class JobFinderControllerTest {
+class JobKoreaJobFinderControllerTest {
 
     @Autowired
     MockMvc mockMvc;
