b3fce88a8980c1a1ec5eb5d0f6eb77494c919ef1
wlsh44 <k87913j@gmail.com>
2022-06-22 01:29:57 +0900

    feat: findById 메서드 추가 #13

--- src/main/java/flab/project/jobfinder/repository/MemberRepository.java
+++ src/main/java/flab/project/jobfinder/repository/MemberRepository.java
@@ -2,6 +2,11 @@ package flab.project.jobfinder.repository;
 
 import flab.project.jobfinder.dto.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findById(Long id);
 }
