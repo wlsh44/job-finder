13556aa194923de49514a0867e3206ca9ae8a221
wlsh44 <k87913j@gmail.com>
2022-06-23 08:59:47 +0900

    feat: table에 맞게 entity 수정 #13

--- src/main/java/flab/project/jobfinder/dto/user/User.java
+++ src/main/java/flab/project/jobfinder/dto/user/User.java
@@ -13,13 +13,13 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 50, nullable = false, unique = true)
+    @Column(length = 50, nullable = false)
     private String name;
 
     @Column(length = 100, nullable = false, unique = true)
     private String email;
 
-    @Column(length = 255, nullable = false)
+    @Column(length = 255, nullable = false, unique = true)
     private String password;
 
     public User toEntity() {
