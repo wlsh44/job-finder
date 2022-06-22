335f4a35fdd97cf39211810caa8ad9b56ccdfc47
wlsh44 <k87913j@gmail.com>
2022-06-22 18:53:02 +0900

    feat: 로그인 기능 추가 #13

---
+++ src/main/java/flab/project/jobfinder/config/MemberPropertiesConfig.java
@@ -0,0 +1,14 @@
+package flab.project.jobfinder.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "member")
+public class MemberPropertiesConfig {
+    private String loginSessionId;
+}
