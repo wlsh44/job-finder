34763edaa9f8aeaec106e9d13853d6e0e7f807a7
wlsh44 <k87913j@gmail.com>
2022-06-22 16:01:03 +0900

    feat: Member db 설정 추가 #13

--- src/main/java/flab/project/jobfinder/dto/member/Members.java
+++ src/main/java/flab/project/jobfinder/dto/member/Members.java
@@ -13,12 +13,20 @@ public class Members {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 50, nullable = false)
+    @Column(length = 50, nullable = false, unique = true)
     private String name;
 
-    @Column(length = 100, nullable = false)
+    @Column(length = 100, nullable = false, unique = true)
     private String email;
 
     @Column(length = 255, nullable = false)
     private String password;
+
+    public Members toEntity() {
+        return Members.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .build();
+    }
 }
