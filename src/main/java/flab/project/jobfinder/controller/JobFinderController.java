bd0a426d1fff7b5e466b0f4b7f24d7e54476a193
wlsh44 <k87913j@gmail.com>
2022-06-22 17:36:59 +0900

    chore: jobFinderController root url 추가 #13

--- src/main/java/flab/project/jobfinder/controller/JobFinderController.java
+++ src/main/java/flab/project/jobfinder/controller/JobFinderController.java
@@ -15,18 +15,20 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
 @Controller
+@RequestMapping("/job-find")
 @RequiredArgsConstructor
 public class JobFinderController {
 
     private final JobFindFactory jobFindFactory;
 
-    @GetMapping("/job-find")
+    @GetMapping("/")
     public String getJobFindForm(Model model) {
         SearchFormDto searchFormDto = new SearchFormDto();
 
@@ -36,7 +38,7 @@ public class JobFinderController {
         return "form";
     }
 
-    @PostMapping("/job-find")
+    @PostMapping("/")
     public String jobFind(@Valid SearchFormDto searchFormDto, BindingResult bindingResult, Model model) {
         log.debug(searchFormDto.toString());
 
