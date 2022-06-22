8c1b91a095ae6530b8a7d59552b30275af663dd6
wlsh44 <k87913j@gmail.com>
2022-06-22 16:42:53 +0900

    feat: transaction 어노테이션 추가 #13

--- src/main/java/flab/project/jobfinder/service/member/MemberService.java
+++ src/main/java/flab/project/jobfinder/service/member/MemberService.java
@@ -5,6 +5,7 @@ import flab.project.jobfinder.exception.UserNotFoundException;
 import flab.project.jobfinder.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +13,13 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     public Members save() {
         Members member = new Members();
         return memberRepository.save(member);
     }
 
+    @Transactional
     public void delete(Long id) {
         memberRepository.delete(
                 memberRepository.findById(id)
