21ee35b76243e06b3ba7d3d6177d3df8229f33ca
wlsh44 <k87913j@gmail.com>
2022-06-23 09:17:02 +0900

    feat: 비밀번호 암호화 추가 #13

--- src/main/java/flab/project/jobfinder/config/UserPropertiesConfig.java
+++ src/main/java/flab/project/jobfinder/config/UserPropertiesConfig.java
@@ -3,7 +3,10 @@ package flab.project.jobfinder.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Setter
@@ -11,4 +14,9 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "member")
 public class UserPropertiesConfig {
     private String loginSessionId;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
