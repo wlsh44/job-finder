7f74d69e2c558efe257f53be891f19876ea50cee
wlsh44 <k87913j@gmail.com>
2022-06-25 10:04:17 +0900

    fix: user 관련 url 수정 #13

--- src/main/java/flab/project/jobfinder/controller/UserController.java
+++ src/main/java/flab/project/jobfinder/controller/UserController.java
@@ -4,10 +4,14 @@ import flab.project.jobfinder.config.UserPropertiesConfig;
 import flab.project.jobfinder.dto.form.LoginFormDto;
 import flab.project.jobfinder.dto.form.SignUpFormDto;
 import flab.project.jobfinder.dto.user.User;
+import flab.project.jobfinder.enums.Location;
+import flab.project.jobfinder.enums.Platform;
 import flab.project.jobfinder.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +20,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/member")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
@@ -31,7 +36,11 @@ public class UserController {
     }
 
     @PostMapping("/sign-up")
-    public String signUp(@Valid SignUpFormDto signUpFormDto) {
+    public String signUp(@Valid SignUpFormDto signUpFormDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "sign-up";
+        }
         userService.save(signUpFormDto);
         return "redirect:/";
     }
