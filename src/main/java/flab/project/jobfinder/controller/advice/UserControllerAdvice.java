8ed270e0e8551fd942e068f70ea98280769f4f8e
wlsh44 <k87913j@gmail.com>
2022-06-25 11:34:27 +0900

    feat: 회원가입 실패시 advice에서 알맞은 오류 보내도록 변경 #13

---
+++ src/main/java/flab/project/jobfinder/controller/advice/UserControllerAdvice.java
@@ -0,0 +1,59 @@
+package flab.project.jobfinder.controller.advice;
+
+import flab.project.jobfinder.dto.form.LoginFormDto;
+import flab.project.jobfinder.dto.form.SignUpFormDto;
+import flab.project.jobfinder.enums.exception.SignUpFailedErrorCode;
+import flab.project.jobfinder.exception.user.LoginFailedException;
+import flab.project.jobfinder.exception.user.SignUpFailedException;
+import flab.project.jobfinder.exception.user.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static flab.project.jobfinder.enums.exception.SignUpFailedErrorCode.ALREADY_EXISTS_USER;
+import static flab.project.jobfinder.enums.exception.SignUpFailedErrorCode.PASSWORD_CONFIRM_NOT_CORRECT;
+
+@Slf4j
+@ControllerAdvice
+public class UserControllerAdvice {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public String UserNotFoundException(UserNotFoundException e, Model model) {
+        return "/";
+    }
+
+    @ExceptionHandler(LoginFailedException.class)
+    public String LoginFailedException(LoginFailedException e, Model model) {
+        model.addAttribute("loginFormDto", new LoginFormDto());
+        return "redirect:/user/login";
+    }
+
+    @ExceptionHandler(SignUpFailedException.class)
+    public String SignUpFailedException(SignUpFailedException e, Model model) {
+        switch (e.getCode()) {
+            case ALREADY_EXISTS_USER: {
+                log.info("user email: {}", e.getSignUpFormDto().getEmail());
+                SignUpFormDto prevForm = e.getSignUpFormDto();
+                SignUpFormDto signUpFormDto = SignUpFormDto.builder()
+                        .name(prevForm.getName())
+                        .build();
+                model.addAttribute("signUpFormDto", signUpFormDto);
+                model.addAttribute("emailError", ALREADY_EXISTS_USER.errorMsg());
+                break;
+            }
+            case PASSWORD_CONFIRM_NOT_CORRECT: {
+                log.info("password: {}, passwordConfirm: {}", e.getSignUpFormDto().getPassword(), e.getSignUpFormDto().getPasswordConfirm());
+                SignUpFormDto prevForm = e.getSignUpFormDto();
+                SignUpFormDto signUpFormDto = SignUpFormDto.builder()
+                        .email(prevForm.getEmail())
+                        .name(prevForm.getName())
+                        .build();
+                model.addAttribute("signUpFormDto", signUpFormDto);
+                model.addAttribute("passwordError", PASSWORD_CONFIRM_NOT_CORRECT.errorMsg());
+                break;
+            }
+        }
+        return "sign-up";
+    }
+}
