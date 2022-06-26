5970d5f7d173df967b3187cdd275fec42d4c1e73
wlsh44 <k87913j@gmail.com>
2022-06-26 10:17:36 +0900

    refactor: 회원가입 검증 방식 변경 #13

--- src/main/java/flab/project/jobfinder/annotation/UniqueEmail.java
+++ src/main/java/flab/project/jobfinder/annotation/UniqueEmail.java
@@ -1,2 +1,17 @@
-package flab.project.jobfinder.annotation;public @interface UniqueEmail {
+package flab.project.jobfinder.annotation;
+
+import flab.project.jobfinder.service.user.validator.UniqueEmailValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = UniqueEmailValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueEmail {
+    String message() default "이미 존재하는 유저입니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
