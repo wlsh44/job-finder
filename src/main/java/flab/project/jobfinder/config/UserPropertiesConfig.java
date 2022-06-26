b6aaaee15ba03a968378fe690c38098fdac486f9
wlsh44 <k87913j@gmail.com>
2022-06-26 16:49:25 +0900

    feat: IndexController  추가 #13

--- src/main/java/flab/project/jobfinder/config/UserPropertiesConfig.java
+++ src/main/java/flab/project/jobfinder/config/UserPropertiesConfig.java
@@ -5,13 +5,14 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Setter
 @Configuration
-@ConfigurationProperties(prefix = "member")
+@ConfigurationProperties(prefix = "user")
 public class UserPropertiesConfig {
     private String loginSessionId;
 
