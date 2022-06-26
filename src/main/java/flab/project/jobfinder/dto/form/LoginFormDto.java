c935a7ae4f01948fc7c28e2bc707dc5f1e76704a
wlsh44 <k87913j@gmail.com>
2022-06-26 16:49:25 +0900

    feat: IndexController  추가 #13

--- src/main/java/flab/project/jobfinder/dto/form/LoginFormDto.java
+++ src/main/java/flab/project/jobfinder/dto/form/LoginFormDto.java
@@ -4,13 +4,14 @@ import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginFormDto {
-    @Email
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     @NotBlank(message = "이메일을 입력해주세요")
     private String email;
     @NotBlank(message = "비밀번호를 입력해주세요")
