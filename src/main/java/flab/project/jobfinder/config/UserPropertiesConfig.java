751d76ee0c6f1ac5f7d8c433e8e20e9f531bdd2b
wlsh44 <k87913j@gmail.com>
2022-06-28 13:35:28 +0900

    chore: 안쓰는 코드 삭제 및 로깅 수정 #13

--- src/main/java/flab/project/jobfinder/config/UserPropertiesConfig.java
+++ src/main/java/flab/project/jobfinder/config/UserPropertiesConfig.java
@@ -1,11 +1,7 @@
 package flab.project.jobfinder.config;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
--- src/main/java/flab/project/jobfinder/interceptor/AuthInterceptor.java
+++ src/main/java/flab/project/jobfinder/interceptor/AuthInterceptor.java
@@ -1,9 +1,7 @@
 package flab.project.jobfinder.interceptor;
 
-import flab.project.jobfinder.entity.user.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,21 +16,11 @@ public class AuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession(false);
 
-        log.info("접근 url: {}", request.getRequestURI());
         if (session == null || session.getAttribute(LOGIN_SESSION_ID) == null) {
-            log.info("미인증 유저 접근");
+            log.info("미인증 유저 접근, url: {}", request.getRequestURI());
             response.sendRedirect("/login?redirectURL=" + request.getRequestURI());
             return false;
         }
         return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
     }
 }
