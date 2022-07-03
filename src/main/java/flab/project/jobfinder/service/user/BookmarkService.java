6196c9947b6c1ef26fa8b85e4730152cc983f925
wlsh44 <k87913j@gmail.com>
2022-07-03 22:48:48 +0900

    feat: 카테고리 service 추가 #17

--- src/main/java/flab/project/jobfinder/service/user/BookmarkService.java
+++ src/main/java/flab/project/jobfinder/service/user/BookmarkService.java
@@ -1,14 +1,23 @@
 package flab.project.jobfinder.service.user;
 
-import flab.project.jobfinder.dto.bookmark.BookmarkRequestDto;
+import flab.project.jobfinder.dto.bookmark.*;
+import flab.project.jobfinder.dto.recruit.RecruitDto;
 import flab.project.jobfinder.entity.recruit.Category;
+import flab.project.jobfinder.entity.recruit.Recruit;
 import flab.project.jobfinder.entity.user.User;
+import flab.project.jobfinder.exception.bookmark.CategoryNotFoundException;
+import flab.project.jobfinder.exception.bookmark.CreateCategoryFailedException;
+import flab.project.jobfinder.exception.user.UserNotFoundException;
 import flab.project.jobfinder.repository.CategoryRepository;
 import flab.project.jobfinder.repository.RecruitRepository;
+import flab.project.jobfinder.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static flab.project.jobfinder.enums.exception.CreateCategoryFailedErrorCode.ALREADY_EXISTS_CATEGORY;
 
 @Service
 @RequiredArgsConstructor
@@ -16,16 +25,44 @@ public class BookmarkService {
 
     private final RecruitRepository recruitRepository;
     private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
 
-    public List<Category> findCategoryByUser(User user) {
-        return categoryRepository.findByUser(user);
+    public List<CategoryResponseDto> findCategoryByUser(User user) {
+        if (!userRepository.existsById(user.getId())) {
+            throw new UserNotFoundException(user.getId());
+        }
+
+        List<Category> categoryList = categoryRepository.findAllByUser(user);
+        return categoryList.stream()
+                .map(CategoryResponseDto::new)
+                .collect(Collectors.toList());
     }
 
-    public Category createCategory(User user, BookmarkRequestDto dto) {
-        Category category = Category.builder()
-                .name(dto.getName())
-                .user(user)
-                .build();
-        return categoryRepository.save(category);
+    public CategoryResponseDto createCategory(User user, NewCategoryRequestDto dto) {
+        if (categoryRepository.existsByUserIdAndName(user.getId(), dto.getName())) {
+            throw new CreateCategoryFailedException(dto, ALREADY_EXISTS_CATEGORY);
+        }
+
+        Category category = categoryRepository.save(dto.toEntity(user));
+        return new CategoryResponseDto(category);
+    }
+
+    public CategoryResponseDto deleteCategory(User user, DeleteCategoryRequestDto dto) {
+        if (!userRepository.existsById(user.getId())) {
+            throw new UserNotFoundException(user.getId());
+        }
+
+        Category category = findCategoryByUserAndName(user, dto.getName());
+        categoryRepository.delete(category);
+        return new CategoryResponseDto(category);
+    }
+
+    public Category findCategoryByUserAndName(User user, String name) {
+        return categoryRepository.findCategoryByUserAndName(user, name)
+                .orElseThrow(() -> new CategoryNotFoundException(name));
+    }
+
+    public Recruit bookmarkRecruit(User user, RecruitDto dto) {
+        return recruitRepository.save(dto.toEntity());
     }
 }
