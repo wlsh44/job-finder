ca35fa621fa75c30bd9c176d53e7882dc965b882
wlsh44 <k87913j@gmail.com>
2022-07-05 14:57:05 +0900

    feat: 북마크 response code 추가 #17

---
+++ src/main/java/flab/project/jobfinder/enums/bookmark/BookmarkResponseCode.java
@@ -0,0 +1,22 @@
+package flab.project.jobfinder.enums.bookmark;
+
+public enum BookmarkResponseCode {
+    CREATE_BOOKMARK("북마크 생성"),
+    FAILED_CREATE_BOOKMARK("북마크 생성 실패"),
+    DELETE_BOOKMARK("북마크 삭제"),
+    FAILED_DELETE_BOOKMARK("북마크 삭제 실패"),
+    GET_BOOKMARKS("북마크 조회"),
+    FAILED_GET_BOOKMARKS("북마크 조회 실패"),
+    UPDATE_BOOKMARK("북마크 수정"),
+    FAILED_UPDATE_BOOKMARK("북마크 수정 실패");
+
+    private final String message;
+
+    BookmarkResponseCode(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return message;
+    }
+}
