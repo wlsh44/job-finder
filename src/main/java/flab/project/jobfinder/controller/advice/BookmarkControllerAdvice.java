c60fee89677d247cfd7f32fc83ae3172b01d7a60
wlsh44 <k87913j@gmail.com>
2022-07-07 21:59:57 +0900

    feat: bookmark controller advice 검증 예외 추가 #17

--- src/main/java/flab/project/jobfinder/controller/advice/BookmarkControllerAdvice.java
+++ src/main/java/flab/project/jobfinder/controller/advice/BookmarkControllerAdvice.java
@@ -6,8 +6,11 @@ import flab.project.jobfinder.exception.bookmark.CategoryNotFoundException;
 import flab.project.jobfinder.exception.bookmark.CreateCategoryFailedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -15,18 +18,26 @@ import static flab.project.jobfinder.enums.bookmark.CategoryResponseCode.FAILED_
 import static flab.project.jobfinder.enums.bookmark.CategoryResponseCode.FAILED_GET_CATEGORIES;
 
 @Slf4j
+@ResponseBody
 @ControllerAdvice
 public class BookmarkControllerAdvice {
 
     @ExceptionHandler(CreateCategoryFailedException.class)
-    public ResponseDto<CategoryResponseDto> CreateCategoryFailedException(CreateCategoryFailedException e) {
+    public ResponseDto<CategoryResponseDto> createCategoryFailedException(CreateCategoryFailedException e) {
         log.info(e.getMessage());
         return new ResponseDto<>(HttpStatus.BAD_REQUEST, FAILED_CREATE_CATEGORY.message(), null);
     }
 
     @ExceptionHandler(CategoryNotFoundException.class)
-    public ResponseDto<List<CategoryResponseDto>> CategoryNotFoundException(CategoryNotFoundException e) {
+    public ResponseDto<List<CategoryResponseDto>> categoryNotFoundException(CategoryNotFoundException e) {
         log.info(e.getMessage());
         return new ResponseDto<>(HttpStatus.BAD_REQUEST, FAILED_GET_CATEGORIES.message(), null);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ResponseDto<List<CategoryResponseDto>> bindingException(BindException e) {
+        log.info(e.getMessage());
+        return new ResponseDto<>(HttpStatus.BAD_REQUEST, FAILED_CREATE_CATEGORY.message(), null);
     }
 }
