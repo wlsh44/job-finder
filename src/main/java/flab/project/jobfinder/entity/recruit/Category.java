048bef353e56eca5e9907817c40bf6f1d88be9f6
wlsh44 <k87913j@gmail.com>
2022-07-11 18:00:08 +0900

    fix: category에서 recruit 삭제시 db 삭제되도록 수정 #17

--- src/main/java/flab/project/jobfinder/entity/recruit/Category.java
+++ src/main/java/flab/project/jobfinder/entity/recruit/Category.java
@@ -32,6 +32,6 @@ public class Category {
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
 
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Recruit> recruits = new ArrayList<>();
 }
