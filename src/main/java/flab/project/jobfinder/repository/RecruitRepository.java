3d8dd2f5529ffbee8bd0306b88dabf15327ec978
wlsh44 <k87913j@gmail.com>
2022-07-13 00:43:46 +0900

    chore: 필요 없는 코드 삭제 #17

--- src/main/java/flab/project/jobfinder/repository/RecruitRepository.java
+++ src/main/java/flab/project/jobfinder/repository/RecruitRepository.java
@@ -1,14 +1,10 @@
 package flab.project.jobfinder.repository;
 
-import flab.project.jobfinder.entity.recruit.Category;
 import flab.project.jobfinder.entity.recruit.Recruit;
-import flab.project.jobfinder.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface RecruitRepository extends JpaRepository<Recruit, Long> {
-    List<Recruit> findAllByCategory(Category category);
 }
