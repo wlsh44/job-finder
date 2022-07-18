47f91b949b4f4e4be580f6fefc3196d07978ed0b
wlsh44 <k87913j@gmail.com>
2022-07-18 18:07:05 +0900

    style: 태그 관련 프런트 추가 #17

--- src/main/resources/static/js/bookmark.js
+++ src/main/resources/static/js/bookmark.js
@@ -50,6 +50,34 @@ function createCategory() {
     rollbackModalFooter()
 }
 
+function rollbackModalFooter() {
+    let addButton = '<button id="add-category-button" class="btn btn-primary" onclick=addCategory()>카테고리 추가</button>' +
+        '<button id="bookmark-button" class="btn btn-primary" onclick=bookmarkRecruit()>북마크 추가</button>'
+    $("#bookmark-footer").html(addButton)
+}
+
+function addCategory() {
+    let addCategory = '<div id="add-category">' +
+        '<input type="text" id="add-input">' +
+        '<button class="btn btn-secondary" onclick="rollbackModalFooter()">취소</button>' +
+        '<button id="add-category" class="btn btn-primary" onclick="createCategory()">저장</button>' +
+        '</div>'
+    $("#bookmark-footer").html(addCategory)
+}
+
+function deleteCategory(deleteId) {
+    $.ajax({
+        url: `/my-page/bookmark/?categoryId=${deleteId}`,
+        type: "DELETE",
+    }).done(function (fragment) {
+        alert("삭제되었습니다.")
+        $("#categoryList").replaceWith(fragment);
+    }).fail(function(data) {
+        alert("삭제에 실패했습니다.")
+        console.log(data)
+    });
+}
+
 function bookmarkRecruit() {
     let arr = []
     $(".bookmark-input").map(function (idx, ele) {
@@ -84,34 +112,6 @@ function bookmarkRecruit() {
     })
 }
 
-function rollbackModalFooter() {
-    let addButton = '<button id="add-category-button" class="btn btn-primary" onclick=addCategory()>카테고리 추가</button>' +
-        '<button id="bookmark-button" class="btn btn-primary" onclick=bookmarkRecruit()>북마크 추가</button>'
-    $("#bookmark-footer").html(addButton)
-}
-
-function addCategory() {
-    let addCategory = '<div id="add-category">' +
-        '<input type="text" id="add-input">' +
-        '<button class="btn btn-secondary" onclick="rollbackModalFooter()">취소</button>' +
-        '<button id="add-category" class="btn btn-primary" onclick="createCategory()">저장</button>' +
-        '</div>'
-    $("#bookmark-footer").html(addCategory)
-}
-
-function deleteCategory(deleteId) {
-    $.ajax({
-        url: `/my-page/bookmark/?categoryId=${deleteId}`,
-        type: "DELETE",
-    }).done(function (fragment) {
-        alert("삭제되었습니다.")
-        $("#categoryList").replaceWith(fragment);
-    }).fail(function(data) {
-        alert("삭제에 실패했습니다.")
-        console.log(data)
-    });
-}
-
 function deleteBookmark(deleteId, categoryId) {
     $.ajax({
         url: `/my-page/bookmark/${categoryId}?bookmarkId=${deleteId}`,
@@ -123,4 +123,96 @@ function deleteBookmark(deleteId, categoryId) {
         alert("삭제에 실패했습니다.")
         console.log(data)
     });
+}
+
+function popTagOpen(recruit) {
+    if (!isUserExists) {
+        alert("로그인이 필요합니다.")
+        window.location.href = "/login"
+    }
+    console.log(recruit);
+    bookmarkNum = recruit
+
+    $('#bookmark-modal').modal('show');
+
+    getTagList()
+}
+
+function getTagList() {
+    $.ajax({
+        type: "GET",
+        url: "/tag",
+        dataType: "text",
+    }).done(function (fragment) {
+        $("#tagList").replaceWith(fragment);
+    });
+}
+
+function rollbackTagModalFooter() {
+    let addButton = '<button type="button" id="new-tag-button" class="btn btn-primary" onClick="createTagForm()">새 태그</button>' +
+                    '<button type="button" id="add-tag-button" class="btn btn-primary" onClick="addTag()">태그 추가</button>'
+    $("#tag-footer").html(addButton)
+}
+
+function createTagForm() {
+    let createTag = '<div id="create-tag">' +
+        '<input type="text" id="create-tag-input">' +
+        '<button class="btn btn-secondary" onclick="rollbackTagModalFooter()">취소</button>' +
+        '<button id="create-tag" class="btn btn-primary" onclick="createTag()">저장</button>' +
+        '</div>'
+    $("#tag-footer").html(createTag)
+}
+
+function createTag() {
+    var name = $("#create-tag-input").val();
+
+    var newTagRequestDto = {
+        name: name
+    }
+
+    $.ajax({
+        type: "POST",
+        url: `/tag`,
+        data: newTagRequestDto
+    }).done(function (fragment) {
+        $("#tagList").replaceWith(fragment);
+    }).fail(function (xhr, status) {
+        console.log(xhr)
+        alert(xhr.responseJSON["message"])
+    });
+
+    rollbackTagModalFooter()
+}
+
+function addTag() {
+    let arr = []
+    $(".tag-input").map(function (idx, ele) {
+        if ($(ele).is(":checked")) {
+            let tagName = $(ele).next("label").text().trim();
+            arr.push(tagName)
+        }
+    });
+
+    for (const arrElement of arr) {
+        console.log(arrElement)
+    }
+
+    let taggingRequestDto = {
+        "tagList": arr
+    }
+    console.log(taggingRequestDto)
+
+    $.ajax({
+        type: "PUT",
+        url: `/tag?bookmarkId=${bookmarkNum}`,
+        contentType: "application/json",
+        data: JSON.stringify(taggingRequestDto)
+    }).done(function (fragment) {
+        alert("태그가 추가되었습니다.")
+        window.location.reload()
+        popClose()
+    }).fail(function (data) {
+        console.log(data)
+        alert("태그 추가에 실패했습니다.")
+    })
 }
