e2f9bf9bd8a55feeee1ce56d20a6128e036a47b6
wlsh44 <k87913j@gmail.com>
2022-07-20 11:44:23 +0900

    chore: html, js 코드 정리 #17

--- src/main/resources/static/js/bookmark.js
+++ src/main/resources/static/js/bookmark.js
@@ -1,17 +1,17 @@
-var bookmarkNum
-var isUserExists
+var bookmarkNum;
+var isUserExists;
 
 function popOpen(recruit) {
     if (!isUserExists) {
-        alert("로그인이 필요합니다.")
-        window.location.href = "/login"
+        alert("로그인이 필요합니다.");
+        window.location.href = "/login";
     }
     console.log(recruit);
-    bookmarkNum = recruit
+    bookmarkNum = recruit;
 
     $('#bookmark-modal').modal('show');
 
-    getCategoryList()
+    getCategoryList();
 }
 
 function popClose() {
@@ -30,11 +30,11 @@ function getCategoryList() {
 
 function createCategory() {
 
-    var name = $("#add-input").val();
+    const name = $("#add-input").val();
 
-    var newCategoryRequestDto = {
+    const newCategoryRequestDto = {
         name: name
-    }
+    };
 
     $.ajax({
         type: "POST",
@@ -43,26 +43,26 @@ function createCategory() {
     }).done(function (fragment) {
         $("#categoryList").replaceWith(fragment);
     }).fail(function (xhr, status) {
-        console.log(xhr)
-        alert(xhr.responseJSON["message"])
+        console.log(xhr);
+        alert(xhr.responseJSON["message"]);
     });
 
-    rollbackModalFooter()
+    rollbackModalFooter();
 }
 
 function rollbackModalFooter() {
-    let addButton = '<button id="add-category-button" class="btn btn-primary" onclick=addCategory()>카테고리 추가</button>' +
-        '<button id="bookmark-button" class="btn btn-primary" onclick=bookmarkRecruit()>북마크 추가</button>'
-    $("#bookmark-footer").html(addButton)
+    const addButton = '<button id="add-category-button" class="btn btn-primary" onclick=addCategory()>카테고리 추가</button>' +
+        '<button id="bookmark-button" class="btn btn-primary" onclick=bookmarkRecruit()>북마크 추가</button>';
+    $("#bookmark-footer").html(addButton);
 }
 
 function addCategory() {
-    let addCategory = '<div id="add-category">' +
+    const addCategory = '<div id="add-category">' +
         '<input type="text" id="add-input">' +
         '<button class="btn btn-secondary" onclick="rollbackModalFooter()">취소</button>' +
         '<button id="add-category" class="btn btn-primary" onclick="createCategory()">저장</button>' +
-        '</div>'
-    $("#bookmark-footer").html(addCategory)
+        '</div>';
+    $("#bookmark-footer").html(addCategory);
 }
 
 function deleteCategory(deleteId) {
@@ -70,33 +70,33 @@ function deleteCategory(deleteId) {
         url: `/my-page/bookmark/?categoryId=${deleteId}`,
         type: "DELETE",
     }).done(function (fragment) {
-        alert("삭제되었습니다.")
+        alert("삭제되었습니다.");
         $("#categoryList").replaceWith(fragment);
     }).fail(function(data) {
-        alert("삭제에 실패했습니다.")
-        console.log(data)
+        alert("삭제에 실패했습니다.");
+        console.log(data);
     });
 }
 
 function bookmarkRecruit() {
-    let arr = []
+    const arr = [];
     $(".bookmark-input").map(function (idx, ele) {
         if ($(ele).is(":checked")) {
-            let categoryName = $(ele).next("label").text().trim();
-            arr.push(categoryName)
+            const categoryName = $(ele).next("label").text().trim();
+            arr.push(categoryName);
         }
     });
 
     for (const arrElement of arr) {
-        console.log(arrElement)
+        console.log(arrElement);
     }
-    console.log(recruitDtoList[bookmarkNum])
+    console.log(recruitDtoList[bookmarkNum]);
 
-    let newBookmarkRequestDto = {
+    const newBookmarkRequestDto = {
         categoryList: arr,
         recruitDto: recruitDtoList[bookmarkNum]
-    }
-    console.log(newBookmarkRequestDto)
+    };
+    console.log(newBookmarkRequestDto);
 
     $.ajax({
         type: "POST",
@@ -104,11 +104,11 @@ function bookmarkRecruit() {
         contentType: "application/json",
         data: JSON.stringify(newBookmarkRequestDto)
     }).done(function () {
-        alert("북마크 되었습니다.")
-        popClose()
+        alert("북마크 되었습니다.");
+        popClose();
     }).fail(function (data) {
-        console.log(data)
-        alert("북마크에 실패했습니다.")
+        console.log(data);
+        alert("북마크에 실패했습니다.");
     })
 }
 
@@ -117,25 +117,25 @@ function deleteBookmark(deleteId, categoryId) {
         url: `/my-page/bookmark/${categoryId}?bookmarkId=${deleteId}`,
         type: "DELETE",
     }).done(function (fragment) {
-        alert("삭제되었습니다.")
+        alert("삭제되었습니다.");
         $("#bookmarkList").replaceWith(fragment);
     }).fail(function(data) {
-        alert("삭제에 실패했습니다.")
-        console.log(data)
+        alert("삭제에 실패했습니다.");
+        console.log(data);
     });
 }
 
 function popTagOpen(recruit) {
     if (!isUserExists) {
-        alert("로그인이 필요합니다.")
-        window.location.href = "/login"
+        alert("로그인이 필요합니다.");
+        window.location.href = "/login";
     }
     console.log(recruit);
-    bookmarkNum = recruit
+    bookmarkNum = recruit;
 
     $('#bookmark-modal').modal('show');
 
-    getTagList()
+    getTagList();
 }
 
 function getTagList() {
@@ -149,26 +149,26 @@ function getTagList() {
 }
 
 function rollbackTagModalFooter() {
-    let addButton = '<button type="button" id="new-tag-button" class="btn btn-primary" onClick="createTagForm()">새 태그</button>' +
-                    '<button type="button" id="add-tag-button" class="btn btn-primary" onClick="addTag()">태그 추가</button>'
-    $("#tag-footer").html(addButton)
+    const addButton = '<button type="button" id="new-tag-button" class="btn btn-primary" onClick="createTagForm()">새 태그</button>' +
+        '<button type="button" id="add-tag-button" class="btn btn-primary" onClick="addTag()">태그 추가</button>';
+    $("#tag-footer").html(addButton);
 }
 
 function createTagForm() {
-    let createTag = '<div id="create-tag">' +
+    const createTag = '<div id="create-tag">' +
         '<input type="text" id="create-tag-input">' +
         '<button class="btn btn-secondary" onclick="rollbackTagModalFooter()">취소</button>' +
         '<button id="create-tag" class="btn btn-primary" onclick="createTag()">저장</button>' +
-        '</div>'
-    $("#tag-footer").html(createTag)
+        '</div>';
+    $("#tag-footer").html(createTag);
 }
 
 function createTag() {
-    var name = $("#create-tag-input").val();
+    const name = $("#create-tag-input").val();
 
-    var newTagRequestDto = {
+    const newTagRequestDto = {
         name: name
-    }
+    };
 
     $.ajax({
         type: "POST",
@@ -177,30 +177,30 @@ function createTag() {
     }).done(function (fragment) {
         $("#tagList").replaceWith(fragment);
     }).fail(function (xhr, status) {
-        console.log(xhr)
-        alert(xhr.responseJSON["message"])
+        console.log(xhr);
+        alert(xhr.responseJSON["message"]);
     });
 
-    rollbackTagModalFooter()
+    rollbackTagModalFooter();
 }
 
 function addTag() {
-    let arr = []
+    let arr = [];
     $(".tag-input").map(function (idx, ele) {
         if ($(ele).is(":checked")) {
-            let tagName = $(ele).next("label").text().trim();
-            arr.push(tagName)
+            const tagName = $(ele).next("label").text().trim();
+            arr.push(tagName);
         }
     });
 
     for (const arrElement of arr) {
-        console.log(arrElement)
+        console.log(arrElement);
     }
 
-    let taggingRequestDto = {
+    const taggingRequestDto = {
         "tagList": arr
-    }
-    console.log(taggingRequestDto)
+    };
+    console.log(taggingRequestDto);
 
     $.ajax({
         type: "PUT",
@@ -208,20 +208,20 @@ function addTag() {
         contentType: "application/json",
         data: JSON.stringify(taggingRequestDto)
     }).done(function (fragment) {
-        alert("태그가 추가되었습니다.")
-        window.location.reload()
-        popClose()
+        alert("태그가 추가되었습니다.");
+        window.location.reload();
+        popClose();
     }).fail(function (data) {
-        console.log(data)
-        alert("태그 추가에 실패했습니다.")
-    })
+        console.log(data);
+        alert("태그 추가에 실패했습니다.");
+    });
 }
 
 function untag(bookmarkNum, tagId) {
 
     const unTagRequestDto = {
         tagId: tagId
-    }
+    };
 
     $.ajax({
         type: "DELETE",
@@ -229,12 +229,12 @@ function untag(bookmarkNum, tagId) {
         contentType: "application/json",
         data: JSON.stringify(unTagRequestDto)
     }).done(function (fragment) {
-        alert("태그가 삭제되었습니다.")
-        window.location.reload()
+        alert("태그가 삭제되었습니다.");
+        window.location.reload();
     }).fail(function (data) {
-        console.log(data)
-        alert("태그 삭제에 실패했습니다.")
-        window.location.reload()
+        console.log(data);
+        alert("태그 삭제에 실패했습니다.");
+        window.location.reload();
     })
 }
 
@@ -243,10 +243,10 @@ function removeTag(tagId) {
         type: "DELETE",
         url: `/tag?tagId=${tagId}`
     }).done(function (fragment) {
-        alert("태그가 삭제되었습니다.")
+        alert("태그가 삭제되었습니다.");
         $("#tagList").replaceWith(fragment);
     }).fail(function (data) {
-        console.log(data)
-        alert("태그 삭제에 실패했습니다.")
+        console.log(data);
+        alert("태그 삭제에 실패했습니다.");
     });
 }
