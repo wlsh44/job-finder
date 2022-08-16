321b4ec3b5a2b515b558c2fe81f378d09714bf42
wlsh44 <k87913j@gmail.com>
2022-08-16 17:08:45 +0900

    test: 카테고리 repository 테스트 코드 추가 #17

--- src/test/java/flab/project/jobfinder/repository/CategoryRepositoryTest.java
+++ src/test/java/flab/project/jobfinder/repository/CategoryRepositoryTest.java
@@ -2,17 +2,18 @@ package flab.project.jobfinder.repository;
 
 import flab.project.jobfinder.entity.recruit.Category;
 import flab.project.jobfinder.entity.user.User;
+import flab.project.jobfinder.service.user.CategoryService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CategoryRepositoryTest {
 
@@ -23,62 +24,86 @@ class CategoryRepositoryTest {
     UserRepository userRepository;
 
     User user;
-
+    Category saveCategory;
+    String categoryName;
 
     @BeforeEach
-    void cleanBeforeTest() {
-        userRepository.deleteAll();
-        categoryRepository.deleteAll();
+    void init() {
         user = User.builder()
-                .name("test")
+                .name("test1")
                 .password("password")
-                .email("test@test.test")
+                .email("test1@test.test")
+                .categories(new ArrayList<>())
+                .recruits(new ArrayList<>())
                 .build();
         user = userRepository.save(user);
-    }
-
-    @AfterEach
-    void cleanAfterTest() {
-        userRepository.deleteAll();
-        categoryRepository.deleteAll();
+        categoryName = "testCategory";
+        Category category = Category.builder()
+                .user(user)
+                .name(categoryName).build();
+        saveCategory = categoryRepository.save(category);
     }
 
     @Test
     @DisplayName("전체 카테고리 조회 테스트")
-    void findAllByUserTest() {
+    void findByUserTest() {
         //given
         Category category1 = Category.builder()
                 .user(user)
-                .name("category1").build();
+                .name("testCategory1")
+                .recruits(new ArrayList<>()).build();
         Category category2 = Category.builder()
                 .user(user)
-                .name("category2").build();
+                .name("testCategory2")
+                .recruits(new ArrayList<>()).build();
         Category category3 = Category.builder()
                 .user(user)
-                .name("category3").build();
-        categoryRepository.saveAll(List.of(category1, category2, category3));
+                .name("testCategory3")
+                .recruits(new ArrayList<>()).build();
+        List<Category> categoryList = List.of(saveCategory, category1, category2, category3);
+        categoryRepository.saveAll(categoryList);
 
         //when
-        List<Category> categories = categoryRepository.findAllByUser(user);
+        List<Category> categories = categoryRepository.findByUser(user);
 
         //then
-        assertThat(categories.size()).isEqualTo(3);
-        assertThat(categories.contains(category1)).isTrue();
+        assertThat(categories).isEqualTo(categoryList);
+    }
+
+    @Test
+    @DisplayName("카테고리 다중 조회 테스트")
+    void findByNameInTest() {
+        //given
+        Category category1 = Category.builder()
+                .user(user)
+                .name("testCategory1")
+                .recruits(new ArrayList<>()).build();
+        Category category2 = Category.builder()
+                .user(user)
+                .name("testCategory2")
+                .recruits(new ArrayList<>()).build();
+        Category category3 = Category.builder()
+                .user(user)
+                .name("testCategory3")
+                .recruits(new ArrayList<>()).build();
+        List<Category> categoryList = List.of(category1, category2, category3);
+        categoryRepository.saveAll(categoryList);
+        List<String> categoryNameList = List.of("testCategory1", "testCategory2", "testCategory3");
+
+        //when
+        List<Category> categories = categoryRepository.findByNameIn(categoryNameList);
+
+        //then
+        assertThat(categories).isEqualTo(categoryList);
     }
 
     @Test
     @DisplayName("특정 카테고리 조회 테스트 - id")
-    void findByUserAndIdTest() {
+    void findCategoryTest() {
         //given
-        String categoryName = "category";
-        Category category = Category.builder()
-                .user(user)
-                .name(categoryName)
-                .build();
-        Category saveCategory = categoryRepository.save(category);
 
         //when
-        Category res = categoryRepository.findByUserAndId(user, saveCategory.getId()).get();
+        Category res = categoryRepository.findCategory(user, saveCategory.getId()).get();
 
         //then
         assertThat(res.getName()).isEqualTo(categoryName);
@@ -89,15 +114,9 @@ class CategoryRepositoryTest {
     @DisplayName("특정 카테고리 조회 테스트 - 이름")
     void findByUserAndNameTest() {
         //given
-        String categoryName = "category";
-        Category category = Category.builder()
-                .user(user)
-                .name(categoryName)
-                .build();
-        categoryRepository.save(category);
 
         //when
-        Category res = categoryRepository.findByUserAndName(user, categoryName).get();
+        Category res = categoryRepository.findCategory(user, categoryName).get();
 
         //then
         assertThat(res.getName()).isEqualTo(categoryName);
@@ -108,11 +127,6 @@ class CategoryRepositoryTest {
     @DisplayName("특정 카테고리 조회 체크 테스트 - 이름")
     void existsByUserAndNameTest() {
         //given
-        String categoryName = "category";
-        Category category = Category.builder()
-                .user(user)
-                .name(categoryName).build();
-        categoryRepository.save(category);
 
         //when
         boolean res = categoryRepository.existsByUserAndName(user, categoryName);
@@ -125,16 +139,23 @@ class CategoryRepositoryTest {
     @DisplayName("특정 카테고리 조회 체크 테스트 - id")
     void existsByUserAndIdTest() {
         //given
-        String categoryName = "category";
-        Category category = Category.builder()
-                .user(user)
-                .name(categoryName).build();
-        Category save = categoryRepository.save(category);
 
         //when
-        boolean res = categoryRepository.existsByUserAndId(user, save.getId());
+        boolean res = categoryRepository.existsByUserAndId(user, saveCategory.getId());
 
         //then
         assertThat(res).isTrue();
+    }
+
+    @Test
+    @DisplayName("카테고리 수 확인")
+    void countByUser() {
+        //given
+
+        //when
+        int categoryNum = categoryRepository.countByUser(user);
+
+        //given
+        assertThat(categoryNum).isEqualTo(1);
     }
 }
